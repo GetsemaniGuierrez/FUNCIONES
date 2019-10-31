da9de82ad0a2827071a33232dc0a3eaec60744fd
Alumno <Alumno@PC49>
2019-10-31 12:06:34 -0600

    jygy

---
+++ 311020/311020/311020.cpp
@@ -0,0 +1,14 @@
+#include<iostream> 
+#include<string>
+using namespace std;
+
+int main()
+{
+	int a, b, suma;
+	cout << "	Primer valor: ";
+	cin >> a;
+	cout << "	Segundo valor: ";
+	cin >> b;
+	suma = a + b;
+	cout
+}
